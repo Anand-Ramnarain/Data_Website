48700e5d5967c26a7dd0906a931ec73a6b9c376a
Anand-Ramnarain <125869920+Anand-Ramnarain@users.noreply.github.com>
2023-09-09 11:25:54 +0200

    removed conflics with nav bar

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -86,8 +86,10 @@ document.addEventListener("DOMContentLoaded", function () {
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
-  header.classList.toggle("sticky", this.window.scrollY > 80);
+  header.classList.toggle("sticky", window.scrollY > 80);
+  header.classList.toggle("shadow", window.scrollY > 0);
 });
+
 
 document.addEventListener("DOMContentLoaded", function () {
   let menu = document.querySelector("#menu-icon");
